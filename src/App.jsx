625deeb4aa47ffdf8227e6b5fb2ac1fa5393baa6
Horushs11 <soyroberto2002@gmail.com>
2025-09-04 14:22:24 +0200

    vercel analytics v2

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ export default function App() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 via-indigo-950 to-gray-900">
       <div className="p-20 max-w-[1500px] mx-auto">
-      <Routes>
         <Analytics />
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/team/:id" element={<Team />} />
       </Routes>
